962fca3e263f7943fa10cc4b24ecc9c3f476cb8c
hank9999 <hank9999@qq.com>
2023-01-06 12:37:24 +0800

    JSON Utils

---
+++ src/main/java/com/github/hank9999/blockhunt/json/JSON.kt
@@ -0,0 +1,41 @@
+package com.github.hank9999.blockhunt.json
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.*
+
+class JSON {
+
+    object Extension {
+        operator fun JsonElement.get(i: String): JsonElement {
+            return jsonObject[i]!!
+        }
+
+        val JsonElement.String: String
+            get() { return this.jsonPrimitive.content }
+
+        val JsonElement.Int: Int
+            get() { return this.jsonPrimitive.int }
+
+        val JsonElement.Bool: Boolean
+            get() { return this.jsonPrimitive.boolean }
+
+        val JsonElement.Long: Long
+            get() { return this.jsonPrimitive.long }
+
+        val JsonElement.Double: Double
+            get() { return this.jsonPrimitive.double }
+
+        val JsonElement.Float: Float
+            get() { return this.jsonPrimitive.float }
+    }
+
+    companion object {
+        @OptIn(ExperimentalSerializationApi::class)
+        val json = Json {
+            ignoreUnknownKeys = true
+            classDiscriminator = "#class"
+            explicitNulls = false
+        }
+
+    }
+}
